3d2132cb7d1dc052a2782df469c7b5d804ac2bc5
mmuhofy <muhofy.social@gmail.com>
2026-05-25 14:36:24 +0300

    feat(di): add datastore hilt module

---
+++ app/src/main/java/com/still/app/di/DataStoreModule.kt
@@ -0,0 +1,27 @@
+package com.still.app.di
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import com.still.app.util.Constants
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(
+    name = Constants.DATASTORE_PREFERENCES_NAME,
+)
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataStoreModule {
+
+    @Provides
+    @Singleton
+    fun provideDataStore(@ApplicationContext context: Context): DataStore<Preferences> =
+        context.dataStore
+}
